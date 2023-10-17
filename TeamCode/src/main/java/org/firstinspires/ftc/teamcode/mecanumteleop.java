85acd5a7386e0a6b9f4835338152178c583f83d2
sudomaster9865 <98765847+sudomaster9865@users.noreply.github.com>
2023-10-17 10:33:40 -0500

    Update mecanumteleop.java
    Added boost button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumteleop.java
@@ -48,15 +48,23 @@ public class mecanumteleop extends LinearOpMode {
             //double rt = gamepad1.right_trigger;
             //double lt = gamepad1.left_trigger;
 
-            //double lbp = 2;
+            double rbp = 1;
             //double rbp = 0.30;
-            //if(buttonClick(lb)) {
-            //    lbp -= 1.5;
-            //}
+            if(buttonClick(rb)) {
+                if(rbp == 2.0) {
+                    rbp = 2;
+                }
+                else {
+                    rbp += .01;
+                }
+            }
+            else {
+                rbp = 1;
+            }
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            double denominator = 1;
+            double denominator = 1/2 * rbp;
             double frontLeftPower = ((y + x + rx) / denominator);
             double backLeftPower = ((y - x + rx) / denominator);
             double frontRightPower = ((y - x - rx) / denominator);
